1cfa0c0b949dd1188cdf6299458a7a13cdaf58b1
Jack09jk <57107195+Jack09jk@users.noreply.github.com>
2020-05-19 22:28:16 +0530

    Update autonamerabbit.py

--- stdplugins/autonamerabbit.py
+++ stdplugins/autonamerabbit.py
@@ -18,7 +18,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"⏳{HM}⏳❂ ▄︻デ©O®O₦₳ ═━一 ❂📅{DM}📅"
+        name = f"⏳{HM}⏳❂ Adͥriͣaͫn oɯɐꞀ  ❂📅{DM}📅"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
